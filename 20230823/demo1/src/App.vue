f7c92d781b1d8be9f77854837eb991e9330ee76d
Dys111AAA <d15831519925@163.com>
2023-08-23 21:22:12 +0800

    20230823提交

---
+++ 20230823/demo1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="left">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/shopcar">shopcar</router-link>
+      </nav>
+    </div>
+    <div class="right">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  width: 100vw;
+  .left{
+    width: 250px;
+    background: pink;
+    height: 100vh;
+  }
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ 20230823/demo1/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="box">
+      aaaa
+      <button>按钮</button>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+  @size:200px;
+  .box{
+    font-size: @size;
+    button{
+      background: red;
+    }
+  }
+</style>
+
